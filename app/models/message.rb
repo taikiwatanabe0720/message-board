99bb475cb50d0f0829d5b697bfca5efc72ba48c2
taiki watanabe <taikiwata0720@gmail.com>
2017-03-22 06:16:54 +0000

    Added age column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,11 @@
 class Message < ActiveRecord::Base
   #名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence:true
+  #年齢は必須入力かつ0以上かつ150以下の数字
+  validates :age , presence:true,
+                   numericality: { only_integer: true, 
+                                  greater_than_or_equal_to: 0,
+                                  less_than_or_equal_to:150 }
   #内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum:2, maximum: 20 } , presence:true
 end
